fcc5431f6fd78a9ca6603e8e20adf56342be008b
Phonedolly <c1john0803@gmail.com>
2022-08-12 07:33:34 +0900

    apply markdown

--- client/src/Components/Art.jsx
+++ client/src/Components/Art.jsx
@@ -1,34 +1,68 @@
-export default function Art() {
-  return (
-    <div>
-      On the other hand, we denounce with righteous indignation and dislike men
-      who are so beguiled and demoralized by the charms of pleasure of the
-      moment, so blinded by desire, that they cannot foresee the pain and
-      trouble that are bound to ensue; and equal blame belongs to those who fail
-      in their duty through weakness of will, which is the same as saying
-      through shrinking from toil and pain. These cases are perfectly simple and
-      easy to distinguish. In a free hour, when our power of choice is
-      untrammelled and when nothing prevents our being able to do what we like
-      best, every pleasure is to be welcomed and every pain avoided. But in
-      certain circumstances and owing to the claims of duty or the obligations
-      of business it will frequently occur that pleasures have to be repudiated
-      and annoyances accepted. The wise man therefore always holds in these
-      matters to this principle of selection: he rejects pleasures to secure
-      other greater pleasures, or else he endures pains to avoid worse pains.
-      <br />
-      <br />
-      At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis
-      praesentium voluptatum deleniti atque corrupti quos dolores et quas
-      molestias excepturi sint occaecati cupiditate non provident, similique
-      sunt in culpa qui officia deserunt mollitia animi, id est laborum et
-      dolorum fuga. Et harum quidem rerum facilis est et expedita distinctio.
-      Nam libero tempore, cum soluta nobis est eligendi optio cumque nihil
-      impedit quo minus id quod maxime placeat facere possimus, omnis voluptas
-      assumenda est, omnis dolor repellendus. Temporibus autem quibusdam et aut
-      officiis debitis aut rerum necessitatibus saepe eveniet ut et voluptates
-      repudiandae sint et molestiae non recusandae. Itaque earum rerum hic
-      tenetur a sapiente delectus, ut aut reiciendis voluptatibus maiores alias
-      consequatur aut perferendis doloribus asperiores repellat.
-    </div>
-  );
+import axios from "axios";
+import { useState, useEffect } from "react";
+import React from "react";
+import ReactMarkDown from "react-markdown";
+import remarkGfm from "remark-gfm";
+
+export default class Art extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      md: null,
+    };
+  }
+
+  componentDidMount() {
+    axios
+      .get(
+        "https://raw.githubusercontent.com/Phonedolly/news2atc/master/README.md"
+      )
+      .then((res) => {
+        console.log(res);
+        this.setState({ md: res.data });
+      });
+  }
+
+  render() {
+    const { md } = this.state;
+    return (
+      <ReactMarkDown
+        children={md}
+        remarkPlugins={[remarkGfm]}
+        components={{
+          p({ node, children, ...props }) {
+            return (
+              <div
+                style={{
+                  lineHeight: "1.7em",
+                }}
+              >
+                {children}
+              </div>
+            );
+          },
+          table({ node, children, ...props }) {
+            return (
+              <div style={{ background: "#f0f0f0", padding: "1px 15px" }}>
+                {children}
+              </div>
+            );
+          },
+          blockquote({ node, children, ...props }) {
+            return (
+              <div
+                style={{
+                  background: "#f0f0f0",
+                  padding: "1em 2em",
+                }}
+                {...props}
+              >
+                {children}
+              </div>
+            );
+          },
+        }}
+      />
+    );
+  }
 }
