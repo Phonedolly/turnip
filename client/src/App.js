9790a054938d486a874a5a31845d9d698f7a45a5
Phonedolly <c1john0803@gmail.com>
2022-08-12 20:26:38 +0900

    install react-router-dom

--- client/src/App.js
+++ client/src/App.js
@@ -4,13 +4,23 @@ import logo from './logo.svg';
 import './App.css';
 import Header from './Components/Header'
 import Art from './Components/Art'
+import { BrowserRouter, Link, Route, Routes } from 'react-router-dom';
+import Writer from './Components/Writer';
 
 function App() {
 
   return (
     <div className="App">
-      <Header />
-      <Art />
+      <BrowserRouter>
+        <Header />
+        <Link to="/writer">go to writer</Link>
+        <Routes>
+          <Route path="/" element={<Art />}></Route>
+
+          <Route path="/writer" element={<Writer />}></Route>
+        </Routes>
+
+      </BrowserRouter>
     </div>
   );
 }
