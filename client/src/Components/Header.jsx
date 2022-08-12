76faa16e7e79c385aaee71254f825f61de3c5e09
Phonedolly <c1john0803@gmail.com>
2022-08-12 20:27:54 +0900

    change tag of header to Link

--- client/src/Components/Header.jsx
+++ client/src/Components/Header.jsx
@@ -1,13 +1,14 @@
 import "./Header.scss";
 import Flex from "@react-css/flex";
+import { Link } from "react-router-dom";
 
 export default function Header() {
   return (
-    <a className="header" href="/">
+    <Link className="header" to="/">
       <Flex className="headerText" flexDirection="row" justifyContent="start">
         <div>Stardue</div>
         <div className="underLine" />
       </Flex>
-    </a>
+    </Link>
   );
 }
