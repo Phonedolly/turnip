d841b536f251450ec05e550434cc65f22738c94e
Phonedolly <c1john0803@gmail.com>
2022-08-13 21:51:50 +0900

    add support dotenv

--- server/server.js
+++ server/server.js
@@ -1,22 +1,32 @@
 const path = require('path')
-const bodyParser = require('body-parser')
+const dotenv = require('dotenv')
+
+if (process.env.NODE_ENV === 'production') {
+    dotenv.config({ path: path.join(__dirname, '../.env.production') });
+    console.log("MODE: PRODUCTION");
+} else if (process.env.NODE_ENV === 'dev') {
+    dotenv.config({ path: path.join(__dirname, '../.env.dev') });
+    console.log("MODE: DEVELOPMENT");
+} else {
+    throw new Error('process.env.NDOE_ENV is not set');
+}
+
+const bodyParser = require('body-parser');
 const express = require('express');
 const app = express();
 const api = require('./router/api');
 
-const connect = require('./schemas')
-connect()
+const connect = require('./schemas');
+connect();
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
 app.use("/api", api);
 app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, '../public/index.html'))
+    res.sendFile(path.join(__dirname, '../public/index.html'));
 })
 const PORT = 5000;
-
-
 
 app.use((req, res, next) => {
     const error = new Error(`${req.method} ${req.url} 라우터가 없습니다.`);
