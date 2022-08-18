2ffce5cf15d5b3e65f03dce9c3025211739e15e8
Phonedolly <c1john0803@gmail.com>
2022-08-18 13:41:41 +0900

    move crreateUser to auth

--- server/router/api.js
+++ server/router/api.js
@@ -42,36 +42,4 @@ router.get('/post/:postURL', async (req, res) => {
     })
 })
 
-router.post('/createUser', async (req, res) => {
-  passwordHashed = await createHashedPassword(req.body.password)
-  User.create({ id: req.body.id, password: passwordHashed.password, salt: passwordHashed.salt })
-    .then(result => {
-      console.log("유저 생성 성공");
-      console.log(result);
-      res.send("sucesss")
-    },
-      (error) => {
-        console.error("유저 생성 실패")
-        console.error(error);
-        res.send("error")
-      })
-})
-
-const createSalt = () =>
-  new Promise((resolve, reject) => {
-    crypto.randomBytes(64, (err, buf) => {
-      if (err) reject(err);
-      resolve(buf.toString('base64'));
-    });
-  });
-
-const createHashedPassword = (plainPassword) =>
-  new Promise(async (resolve, reject) => {
-    const salt = await createSalt();
-    crypto.pbkdf2(plainPassword, salt, 9999, 64, 'sha512', (err, key) => {
-      if (err) reject(err);
-      resolve({ password: key.toString('base64'), salt });
-    });
-  });
-
 module.exports = router;
