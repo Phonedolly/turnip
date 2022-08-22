0b26697ea9957641f1ada9fd8caf7d065a43dfdf
Phonedolly <c1john0803@gmail.com>
2022-08-22 23:40:51 +0900

    move stateSaver

--- client/src/Hooks/stateSaver.js
+++ client/src/Hooks/stateSaver.js
@@ -1,13 +1,10 @@
 const state = {}
 
 export const saveState = (component, object) => {
-
     state[component] = object
-
 }
 
 export const getState = (component) => {
 
     return state[component]
-
 }
