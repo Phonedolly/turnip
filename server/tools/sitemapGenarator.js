c72b6707f55ffbd8dab6cdb57ab9ff0177cd6276
Phonedolly <c1john0803@gmail.com>
2022-09-03 06:43:57 +0900

    refactoring sitemapGenerator (2)

--- server/tools/sitemapGenarator.js
+++ server/tools/sitemapGenarator.js
@@ -4,6 +4,9 @@ const axios = require('axios');
 const Post = require('../schemas/post');
 
 const sitemapReader = (offset) => {
+  if (!fs.existsSync(path.join(__dirname, `../../client/build/sitemap${offset}.txt`))) {
+    fs.writeFileSync(path.join(__dirname, `../../client/build/sitemap${offset}.txt`), "");
+  }
   return fs.readFileSync(path.join(__dirname, `../../client/build/sitemap${offset}.txt`), { encoding: 'utf-8' });
 }
 
@@ -13,37 +16,29 @@ const writer = (content, offset) => {
 
 const runner = async () => {
   const mongoBulkData = await Post.find({})
-  const numOfOffset = Math.ceil(mongoBulkData / 1000)
+  const numOfOffset = Math.ceil(mongoBulkData.length / 1000)
+  console.log('numOfOffset: ' + numOfOffset)
 
   for (let i = 0; i < numOfOffset; i++) {
     const prevData = sitemapReader(i);
-    const content = mongoBulkData.slice(i, mongoBulkData.length % 1000)
-  }
+    const last = (mongoBulkData.length % 1000 === 0) ? (i + 1) * 1000 : i * 1000 + mongoBulkData.length % 1000;
+    const curContent = mongoBulkData.slice(i * 1000, last).reduce((acc, cur) => acc.concat(process.env.WEBSITE_URL + '/post/' + cur.postURL + '\n'), "")
 
+    const sitemapUpdated = prevData !== curContent;
 
+    if (sitemapUpdated || !fs.existsSync(__dirname, `../../client/build/sitemap${i}.txt`)) {
+      writer(curContent, i);
+      if (process.env.NODE_ENV === 'dev') {
+        console.log("sitemap refreshed")
+      }
+    }
 
-  const prevData = fs.readFileSync(path.join(__dirname, '../../client/build/sitemap.txt'), { encoding: 'utf-8' })
-  const bulkData = await Post.find({})
-  let sitemapUpdated = false;
-
-  const content = bulkData.reduce((acc, cur, index) => {
-    return acc.concat(process.env.WEBSITE_URL + '/post/' + cur.postURL + '\n')
-  }, "")
-
-  if (prevData !== content) {
-    sitemapUpdated = true;
-  }
-
-  fs.writeFileSync(path.join(__dirname, '../../client/build/sitemap.txt'), content)
-  if (process.env.NODE_ENV === 'dev') {
-    console.log("sitemap refreshed")
-  }
-
-  if (process.env.NODE_ENV === 'production'
-    && fs.existsSync(path.join(__dirname, '../../client/build/sitemap.txt'))
-    && sitemapUpdated) {
-    axios.get('https://www.google.com/ping?sitemap=https://stardue64.com/sitemap.txt');
-    console.log('send sitemap update ping to google')
+    if (process.env.NODE_ENV === 'production'
+      && fs.existsSync(path.join(__dirname, `../../client/build/sitemap${i}.txt`))
+      && sitemapUpdated) {
+      axios.get(`https://www.google.com/ping?sitemap=https://stardue64.com/sitemap${i}.txt`);
+      console.log(`send sitemap${i} update ping to google`)
+    }
   }
 }
 
